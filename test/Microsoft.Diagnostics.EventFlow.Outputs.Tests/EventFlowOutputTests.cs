a10e23f4b1eab9768b79ed4ba9fd91d03fd17a1b
Fernando Gasperi Jabalera <t-fegasp@microsoft.com>
2017-02-06 11:26:56 -0800

    WIP: Adding EventFlowOutput tests.

--- test/Microsoft.Diagnostics.EventFlow.Outputs.Tests/EventFlowOutputTests.cs
+++ test/Microsoft.Diagnostics.EventFlow.Outputs.Tests/EventFlowOutputTests.cs
@@ -13,10 +13,10 @@ namespace Microsoft.Diagnostics.EventFlow.Outputs.Tests
         {
             // Setup
             Mock<IHealthReporter> healthReporterMock = new Mock<IHealthReporter>();
-            var configurationMock = new Mock<IConfiguration>();
+            Mock<IConfiguration> configurationMock = new Mock<IConfiguration>();
 
             // Execute
-            EventFlowHostOutput sender = new EventFlowHostOutput(configurationMock, healthReporterMock.Object);
+            EventFlowHostOutput sender = new EventFlowHostOutput(configurationMock.Object, healthReporterMock.Object);
 
             // Verify
             Assert.NotNull(sender);
@@ -25,11 +25,11 @@ namespace Microsoft.Diagnostics.EventFlow.Outputs.Tests
         [Fact]
         public void ConstructorShouldRequireHealthReporter()
         {
-            var configurationMock = new Mock<IConfiguration>();
+            Mock<IConfiguration> configurationMock = new Mock<IConfiguration>();
 
             Exception ex = Assert.Throws<ArgumentNullException>(() =>
             {
-                EventFlowHostOutput target = new EventFlowHostOutput(configurationMock, null);
+                EventFlowHostOutput target = new EventFlowHostOutput(configurationMock.Object, null);
             });
 
             Assert.Equal("Value cannot be null.\r\nParameter name: healthReporter", ex.Message);
